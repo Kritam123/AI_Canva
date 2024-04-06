da92d2d1ff1c1f33dd867d87f9e799adbe3d7964
Kritam123 <ktmdah01@gmail.com>
2024-04-06 22:09:29 +0545

    push data

--- lib/database/mongoose.ts
+++ lib/database/mongoose.ts
@@ -1,6 +1,7 @@
 import mongoose, { Mongoose } from "mongoose";
 
-const MONGODB_URL = process.env.DATABASE_URL!;
+const MONGODB_URL = process.env.MONGODB_URL;
+
 interface MongooseConnection {
   conn: Mongoose | null;
   promise: Promise<Mongoose> | null;
@@ -8,7 +9,7 @@ interface MongooseConnection {
 
 let cached: MongooseConnection = (global as any).mongoose;
 
-if (cached.conn) {
+if (!cached) {
   cached = (global as any).mongoose = {
     conn: null,
     promise: null,
@@ -16,14 +17,18 @@ if (cached.conn) {
 }
 
 export const connectToDatabase = async () => {
-  if (cached.promise) return cached.conn;
+  if (cached.conn) return cached.conn;
+
   if (!MONGODB_URL) throw new Error("Missing MONGODB_URL");
+
   cached.promise =
     cached.promise ||
     mongoose.connect(MONGODB_URL, {
-      dbName: "AI_Canva",
+      dbName: "imaginify",
       bufferCommands: false,
     });
+
   cached.conn = await cached.promise;
+
   return cached.conn;
 };
